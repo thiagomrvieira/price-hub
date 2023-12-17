18f8dad8ea254c81d2d05cbc98d565b48535314d
“Thiago <tvieira@pdmfc.com>
2023-12-17 12:30:57 +0000

    feat: Add Console resonsible to import the CSV data to the prices table

---
+++ app/Console/Commands/ImportPricesCsv.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Price;
+use App\Models\Product;
+use App\Models\Account;
+use App\Models\User;
+
+class ImportPricesCsv extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:prices-csv';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import prices from CSV file to the prices table';
+
+    /**
+     * Handle the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $filePath = database_path('seeders/imports/import.csv');
+
+        if (!file_exists($filePath)) {
+            $this->error("CSV file not found at $filePath");
+            return;
+        }
+
+        $this->info("Importing prices from CSV file: $filePath");
+
+        $csvData = array_map('str_getcsv', file($filePath));
+        $header = array_shift($csvData);
+
+        $this->importData($csvData, $header);
+
+        $this->info('Import completed.');
+    }
+
+    /**
+     * Import data from CSV to the prices table.
+     *
+     * @param  array  $csvData
+     * @param  array  $header
+     * @return void
+     */
+    private function importData(array $csvData, array $header): void
+    {
+        foreach ($csvData as $row) {
+            $skuIndex = array_search('sku', $header);
+            $accountRefIndex = array_search('account_ref', $header);
+            $userRefIndex = array_search('user_ref', $header);
+            $quantityIndex = array_search('quantity', $header);
+            $valueIndex = array_search('value', $header);
+
+            $sku = $row[$skuIndex];
+            $accountRef = $row[$accountRefIndex];
+            $userRef = $row[$userRefIndex];
+            $quantity = $row[$quantityIndex];
+            $value = $row[$valueIndex];
+
+            $productId = $this->getIdByColumnValue(Product::class, 'sku', $sku);
+            $accountId = $this->getIdByColumnValue(Account::class, 'external_reference', $accountRef);
+            $userId = $this->getIdByColumnValue(User::class, 'external_reference', $userRef);
+
+            Price::create([
+                'product_id' => $productId,
+                'account_id' => $accountId,
+                'user_id' => $userId,
+                'quantity' => $quantity,
+                'value' => $value,
+            ]);
+        }
+    }
+
+    /**
+     * Get the ID of a model by the value of a specific column.
+     *
+     * @param  string  $modelClass
+     * @param  string  $column
+     * @param  mixed   $value
+     * @return int|null
+     */
+    private function getIdByColumnValue(string $modelClass, string $column, $value): ?int
+    {
+        return $modelClass::where($column, $value)->value('id');
+    }
+}
--- app/Models/Price.php
+++ app/Models/Price.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Price extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'account_id',
+        'user_id',
+        'quantity',
+        'value',
+    ];
 }
