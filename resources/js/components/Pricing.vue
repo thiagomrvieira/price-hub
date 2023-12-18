e51e633d7b50b991d06357bf6c6f8dd7ef748f2b
“Thiago <tvieira@pdmfc.com>
2023-12-18 09:57:41 +0000

    feat: Add product price component

--- resources/js/components/Pricing.vue
+++ resources/js/components/Pricing.vue
@@ -2,7 +2,6 @@
     <div class="max-w-2xl mx-auto bg-white p-8 rounded-md shadow-md">
       <div class="text-2xl font-semibold mb-4">Product Prices</div>
   
-      <!-- Form para inserir parâmetros -->
       <form @submit.prevent="getProductPrice">
         <div class="flex mb-4">
           <label class="flex-1 mr-2">
@@ -17,16 +16,17 @@
         <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-700 transition">Get Price</button>
       </form>
   
-      <div v-if="result">
-        <div class="text-lg font-semibold mt-6">Result:</div>
-        <div>Product SKU: {{ result.sku }}</div>
-        <div>Product best price: ${{ result.price.toFixed(2) }}</div>
-      </div>
+      <ProductPriceResult v-if="result" :result="result" />
     </div>
   </template>
   
   <script>
+  import ProductPriceResult from './ProductPriceResult.vue';
+  
   export default {
+    components: {
+      ProductPriceResult,
+    },
     data() {
       return {
         productCode: '',
@@ -35,21 +35,21 @@
       };
     },
     methods: {
-        async getProductPrice() {
-            try {
-                const response = await axios.get(`/api/v1/prices/${this.productCode}/${this.accountId || ''}`);
-                let data = response.data;
-                this.result = {
-                    sku: data.data.sku,
-                    price: parseFloat(data.data.price),
-                };
-                this.error = null;
-            } catch (error) {
-                this.result = null;
-                this.error = 'Error fetching product price.';
-                console.error('Error fetching product price:', error.message);
-            }
-        },
+      async getProductPrice() {
+        try {
+          const response = await axios.get(`/api/v1/prices/${this.productCode}/${this.accountId || ''}`);
+          let data = response.data;
+          this.result = {
+            sku: data.data.sku,
+            price: parseFloat(data.data.price),
+          };
+          this.error = null;
+        } catch (error) {
+          this.result = null;
+          this.error = 'Error fetching product price.';
+          console.error('Error fetching product price:', error.message);
+        }
+      },
     },
   };
   </script>
---
+++ resources/js/components/ProductPriceResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="text-lg font-semibold mt-6">Result:</div>
+    <div>Product SKU: {{ result.sku }}</div>
+    <div>Product best price: ${{ result.price.toFixed(2) }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    result: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
